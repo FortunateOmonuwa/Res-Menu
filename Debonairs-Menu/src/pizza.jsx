7829c3b18fae4ea13ba3a72a4975a3a17b89a194
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-11-08 14:22:12 +0100

    destructure props

--- Debonairs-Menu/src/pizza.jsx
+++ Debonairs-Menu/src/pizza.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable react/prop-types */
-const Pizza = (props) => {
+const Pizza = ({ photoName, name, ingredients, price, soldOut }) => {
   const isDarkmode = true;
 
   const style = isDarkmode ? { color: "black" } : { color: "red" };
 
   return (
     <div className="pizzas">
-      <img src={props.photoName} alt="" className="pizza-image" />
+      <img src={photoName} alt="" className="pizza-image" />
       <div className="content">
         <div>
-          <h2 style={style}>{props.name}p</h2>
-          <p>{props.ingredients}</p>
+          <h2 style={style}>{name}p</h2>
+          <p>{ingredients}</p>
         </div>
 
-        {props.soldOut ? (
+        {soldOut ? (
           <div style={{ color: "red", display: "grid" }}>Sold Out</div>
         ) : (
-          <span>{props.price + 3}</span>
+          <span>{price + 3}</span>
         )}
       </div>
     </div>
